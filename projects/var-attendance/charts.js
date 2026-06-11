02513b633fe706ea61eea32fa6d8c28f1abbc470
AldoVata <90346163+AldoVata@users.noreply.github.com>
2026-06-11 00:31:59 -0400

    Create charts.js

---
+++ projects/var-attendance/charts.js
@@ -0,0 +1,24 @@
+var data = [
+
+{
+
+x: [
+    "Before VAR",
+    "After VAR"
+],
+
+y: [
+    38000,
+    39500
+],
+
+type: "bar"
+
+}
+
+];
+
+Plotly.newPlot(
+    "attendanceChart",
+    data
+);
